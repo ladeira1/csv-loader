a21cb6d281a051175fd063e3c3b2ee3a65f64eec
Joao Ladeira <joaoladeirag@gmail.com>
2023-12-12 16:11:47 -0300

    test: parseCsv

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -2,7 +2,6 @@ module.exports = {
   bail: true,
   clearMocks: true,
   testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.spec.[jt]s?(x)'],
   transform: {
     '^.+\\.ts?$': 'ts-jest',
   },
