4b77b1ce1469004be055e5904d09e9e7aeb38898
Joao Ladeira <joaoladeirag@gmail.com>
2023-12-12 18:22:00 -0300

    config: vercel

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(express.json())
 app.use("/api/", router)
 
-app.get('/hello', (req: Request, res: Response) => {
+app.get('/', (req: Request, res: Response) => {
   return res.send('Express Typescript on Vercel')
 })
 
