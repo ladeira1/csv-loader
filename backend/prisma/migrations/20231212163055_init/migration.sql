edc91abebe3079daba7292dc8240ed42e9aa3fcc
Joao Ladeira <joaoladeirag@gmail.com>
2023-12-12 13:33:20 -0300

    config: backend

---
+++ backend/prisma/migrations/20231212163055_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Person" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "country" TEXT NOT NULL,
+    "favorite_sport" TEXT NOT NULL
+);
---
+++ backend/src/server.ts
@@ -0,0 +1,15 @@
+import express from "express"
+import cors from "cors"
+import { router } from "./routes"
+import "dotenv/config"
+
+const PORT = process.env.API_PORT || 3000
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(router)
+
+app.listen(PORT, () => {
+  console.log('Server is running in PORT ' + PORT)
+})
